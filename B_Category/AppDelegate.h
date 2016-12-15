7079c5b477bc56073d01ac742ec00d8f4773d8e2
Junne <junne.bai@gmail.com>
2016-12-15 17:26:28 +0800

    Initial Commit

---
+++ B_Category/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  B_Category
+//
+//  Created by baijf on 15/12/2016.
+//  Copyright © 2016 Junne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ B_Category/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  B_Category
+//
+//  Created by baijf on 15/12/2016.
+//  Copyright © 2016 Junne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
